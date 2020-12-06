e44071e84d97f069b94835ceebe9516407a9da27
Yash Panchal <72609549+panchaly75@users.noreply.github.com>
2020-12-06 14:48:45 +0530

    Create product.cpp
    A C++ Program to Find the product of two numbers entered by the user.

---
+++ product.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float i,j;
+	cout<<"\nEnter two numbers : ";                                  //Enter two numbers and seprate them by space
+	cin>>i>>j;                                                       //in place of this we also use : cin>>i;cin>>j; but only thing was that you must enter after 1st number then write 2nd and again enter. 
+	cout<<"\nproduct = "<<(i*j);
+	return 0;
+}
