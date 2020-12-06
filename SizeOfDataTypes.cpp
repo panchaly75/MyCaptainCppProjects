cafc8dfec0091a4d5d762ceb1cf27804cd60c9f4
Yash Panchal <72609549+panchaly75@users.noreply.github.com>
2020-12-06 14:31:09 +0530

    Create SizeOfDataTypes.cpp
     A C++ Program to Find Size of int, float, double and char in Your System

---
+++ SizeOfDataTypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n1.\tsize of char : "<<sizeof(char)<<" byte";                      
+	cout<<"\n2.\tsize of int : "<<sizeof(int)<<" bytes";
+	cout<<"\n3.\tsize of float : "<<sizeof(float)<<" bytes";
+	cout<<"\n4.\tsize of double : "<<sizeof(double)<<" bytes\n";
+	
+	return 0;
+}
